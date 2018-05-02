65d5432b7dc2114d44e7f0ac4f35a5ce57b3f8b6
ZoisK <37310332+ZoisK@users.noreply.github.com>
2018-05-02 09:35:30 +0300

    Create run_analysis.R
    empty version - only notes from the assignment on what to do

---
+++ run_analysis.R
@@ -0,0 +1,15 @@
+### 
+### run_analysis.R
+###
+#
+# Scope 
+#
+# 1. Merge the training and the test sets to create one data set.
+# 2. Extract only the measurements on the mean and standard deviation 
+#    for each measurement.
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately label the data set with descriptive variable names.
+# 5. From the data set in step 4, create a second, 
+#    independent tidy data set with 
+#    the average of each variable for each activity and each subject.
+#    a txt file created with write.table() using row.name=FALSE
